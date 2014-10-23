a19a5123873df50130f2207df0e591bd0aa38aeb
Chris Antenesse <chris@antenesse.net>
2014-10-23 11:11:16 -0700

    changes binary name

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,7 +12,7 @@ ark 'packer' do
     url "#{node[:packer][:url_base]}/#{node[:packer][:version]}_#{node[:os]}_#{node[:packer][:arch]}.zip"
     version node[:packer][:version]
     checksum node[:packer][:checksum]
-    has_binaries ["packer"]
+    has_binaries ["packer-packer"]
     append_env_path false
     strip_leading_dir false
 
