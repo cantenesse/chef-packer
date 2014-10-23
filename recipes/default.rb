e32a72066f174adce33721c051fa9f91ce169ce6
Chris Antenesse <chris@antenesse.net>
2014-10-23 14:02:38 -0700

    hardcodes package to download

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,11 +8,14 @@
 # Install packages necessary for extracting stuff
 include_recipe "ark"
 
+packer_url = 'https://dl.bintray.com/mitchellh/packer/packer_0.7.1_linux_amd64.zip'
+packer_checksum = '4f7b8cd96a147f9473cc724397613c8d98bbf0d8cb037b122785ff252eb12581'
+
 ark 'packer' do
-    url "#{node[:packer][:url_base]}/#{node[:packer][:version]}_#{node[:os]}_#{node[:packer][:arch]}.zip"
+    url packer_url 
     version node[:packer][:version]
-    checksum node[:packer][:checksum]
-    has_binaries ["packer-packer"]
+    checksum packer_checksum
+    has_binaries ["packer"]
     append_env_path false
     strip_leading_dir false
 
